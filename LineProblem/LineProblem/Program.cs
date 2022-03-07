d7281167b315b3f63194e9db886600e7786108be
pappurathod <prathod4545@gmail.com>
2022-03-07 19:58:52 +0530

    check equalility of two lines based on end points

--- LineProblem/LineProblem/Program.cs
+++ LineProblem/LineProblem/Program.cs
@@ -16,5 +16,28 @@ Console.WriteLine("enter the value of y2");
 int y2;
 y2 = Convert.ToInt32(Console.ReadLine());
 
-double d =Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) * 1.0);
-Console.WriteLine("Distance is " + d);
+double l1 =Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+Console.WriteLine("length of line 1 is " + l1);
+
+
+
+
+Console.WriteLine("enter the value of x3 ");
+int x3;
+x3 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("enter the value of x4 ");
+int x4;
+x4 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("enter the value of y3 ");
+int y3;
+y3 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("enter the value of y4");
+int y4;
+y4 = Convert.ToInt32(Console.ReadLine());
+
+double l2 = Math.Sqrt(Math.Pow(x4 - x3, 2) + Math.Pow(y4 - y3, 2));
+Console.WriteLine("length of line 2 is " + l2);
+Console.WriteLine("both lines are equal: " + l1.Equals(l2)); //return T/F
